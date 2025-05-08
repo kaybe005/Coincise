b01b58e0c01e22ca6055d681ca5c19f051636e1c
kaybe005 <kalashbijukchhe74@gmail.com>
2025-05-08 23:26:10 +1000

    Fixing typescript bug

--- frontend/components/BudgetAlert.tsx
+++ frontend/components/BudgetAlert.tsx
@@ -1,9 +1,19 @@
-import { HiExclamationCircle } from "react-icons/hi";
+"use client";
+
+import React from "react";
+import { HiExclamationCircle } from "react-icons/hi2";
 
 const BudgetAlert = () => {
   return (
     <div className="flex items-start dark:bg-[#0A2540] dark:text-white gap-3 bg-orange-100 border border-orange-300 text-orange-800 px-4 py-3 rounded-lg text-sm max-w-xs">
-      <HiExclamationCircle size={20} className="mt-1" />
+      <span className="mt-1">
+        {
+          HiExclamationCircle({
+            size: 20,
+            className: "mt-1",
+          }) as unknown as JSX.Element
+        }
+      </span>
       <span>Keep an eye on your spending!</span>
     </div>
   );
